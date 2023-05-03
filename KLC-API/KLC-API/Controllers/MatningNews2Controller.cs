02d1c9f91a23ebb05e4381ec69a8758272dfc40a
ChristianBernheim <bernheim.christian@gmail.com>
2023-05-03 14:18:00 +0200

    fix

---
+++ KLC-API/KLC-API/Controllers/MatningNews2Controller.cs
@@ -0,0 +1,123 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KLC_API.Models;
+
+namespace KLC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatningNews2Controller : ControllerBase
+    {
+        private readonly MatningContext _context;
+
+        public MatningNews2Controller(MatningContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MatningNews2
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MatningNews2>>> GetMatningarNews2()
+        {
+          if (_context.MatningarNews2 == null)
+          {
+              return NotFound();
+          }
+            return await _context.MatningarNews2.ToListAsync();
+        }
+
+        // GET: api/MatningNews2/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MatningNews2>> GetMatningNews2(int id)
+        {
+          if (_context.MatningarNews2 == null)
+          {
+              return NotFound();
+          }
+            var matningNews2 = await _context.MatningarNews2.FindAsync(id);
+
+            if (matningNews2 == null)
+            {
+                return NotFound();
+            }
+
+            return matningNews2;
+        }
+
+        // PUT: api/MatningNews2/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMatningNews2(int id, MatningNews2 matningNews2)
+        {
+            if (id != matningNews2.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(matningNews2).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MatningNews2Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MatningNews2
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<MatningNews2>> PostMatningNews2(MatningNews2 matningNews2)
+        {
+          if (_context.MatningarNews2 == null)
+          {
+              return Problem("Entity set 'MatningContext.MatningarNews2'  is null.");
+          }
+            _context.MatningarNews2.Add(matningNews2);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMatningNews2", new { id = matningNews2.Id }, matningNews2);
+        }
+
+        // DELETE: api/MatningNews2/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMatningNews2(int id)
+        {
+            if (_context.MatningarNews2 == null)
+            {
+                return NotFound();
+            }
+            var matningNews2 = await _context.MatningarNews2.FindAsync(id);
+            if (matningNews2 == null)
+            {
+                return NotFound();
+            }
+
+            _context.MatningarNews2.Remove(matningNews2);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MatningNews2Exists(int id)
+        {
+            return (_context.MatningarNews2?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ KLC-API/KLC-API/Controllers/PatientsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KLC_API.Models;
+
+namespace KLC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly MatningContext _context;
+
+        public PatientsController(MatningContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Patient>>> GetPatienter()
+        {
+          if (_context.Patienter == null)
+          {
+              return NotFound();
+          }
+            return await _context.Patienter.ToListAsync();
+        }
+
+        // GET: api/Patients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Patient>> GetPatient(int id)
+        {
+          if (_context.Patienter == null)
+          {
+              return NotFound();
+          }
+            var patient = await _context.Patienter.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return patient;
+        }
+
+        // PUT: api/Patients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPatient(int id, Patient patient)
+        {
+            if (id != patient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Patients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Patient>> PostPatient(Patient patient)
+        {
+          if (_context.Patienter == null)
+          {
+              return Problem("Entity set 'MatningContext.Patienter'  is null.");
+          }
+            _context.Patienter.Add(patient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPatient", new { id = patient.Id }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePatient(int id)
+        {
+            if (_context.Patienter == null)
+            {
+                return NotFound();
+            }
+            var patient = await _context.Patienter.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patienter.Remove(patient);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PatientExists(int id)
+        {
+            return (_context.Patienter?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
